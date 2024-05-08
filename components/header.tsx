d13dc8126bbf1e2633dd58bffd67bdcbbe6b0b8a
georgetbs <georgekvirkvelia@gmail.com>
2024-05-09 03:39:25 +0400

    next убран

--- components/header.tsx
+++ components/header.tsx
@@ -25,27 +25,25 @@ export async function Header() {
   const cookieStore = cookies()
   const session = await auth({ cookieStore })
 
-  return (
-    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
-      <div className="flex items-center">
-        {session?.user ? (
-          <Sidebar>
-            <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
-              {/* @ts-ignore */}
-              <SidebarList userId={session?.user?.id} />
-            </React.Suspense>
-            <SidebarFooter>
-              <ThemeToggle />
-              <ClearHistory clearChats={clearChats} />
-            </SidebarFooter>
-          </Sidebar>
-        ) : (
-          <Link href="/" target="_blank" rel="nofollow">
-            <IconNextChat className="mr-2 h-6 w-6 dark:hidden" inverted />
-            <IconNextChat className="mr-2 hidden h-6 w-6 dark:block" />
-          </Link>
-        )}
-      </div>
+
+    return (
+      <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
+        <div className="flex items-center">
+          {session?.user ? (
+            <Sidebar>
+              <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
+                {/* @ts-ignore */}
+                <SidebarList userId={session?.user?.id} />
+              </React.Suspense>
+              <SidebarFooter>
+                <ThemeToggle />
+                <ClearHistory clearChats={clearChats} />
+              </SidebarFooter>
+            </Sidebar>
+          ) : null}
+        </div>
+
+    
       
       <div className="flex items-center">
         <Tooltip>
